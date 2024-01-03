e7ec449e7841defce1ff6fa25aba01173964f237
buddejul <buddejul@gmail.com>
2024-01-03 12:03:59 +0100

    Added test for joint computation of late, ols, iv from paper.

--- tests/identification/test_identification.py
+++ tests/identification/test_identification.py
@@ -54,3 +54,25 @@ def test_paper_iv_slope():
     )
 
     np.isclose(actual, expected, atol=1e-4)
+
+
+def test_paper_late_ols_iv():
+    expected = [DGP["late_35_90"], DGP["ols_slope"], DGP["iv_slope"]]
+
+    def _compute(estimand):
+        return _compute_estimand(
+            estimand=estimand,
+            m0=DGP["m0"],
+            m1=DGP["m1"],
+            u_lo=0.35,
+            u_hi=0.9,
+            u_part=[0.35, 0.9],
+            support_z=DGP["support_z"],
+            pscore_z=DGP["pscore_z"],
+            pdf_z=DGP["pdf_z"],
+        )
+
+    actual = [_compute(estimand) for estimand in ["late", "ols_slope", "iv_slope"]]
+
+    # Check that all values are close to expected in one statement
+    np.allclose(actual, expected, atol=1e-4)
