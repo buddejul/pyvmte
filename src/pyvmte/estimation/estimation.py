b4e09f7376f922fb0b30841ec468c4c1f1bf27d4
buddejul <buddejul@gmail.com>
2024-01-04 16:39:23 +0100

    First draft of functions for estimating weights for estimation linear program.

--- src/pyvmte/estimation/estimation.py
+++ src/pyvmte/estimation/estimation.py
@@ -11,6 +11,7 @@ def estimation(
     y_data,
     z_data,
     d_data,
+    tolerance,
     x_data=None,
     u_partition=None,
     analytical_integration=False,
@@ -25,6 +26,7 @@ def estimation(
         y_data (np.array): Array containing the outcome data.
         z_data (np.array): Array containing the instrument data.
         d_data (np.array): Array containing the treatment data.
+        tolerance (float): Tolerance for the second-step linear program.
         x_data (np.array, optional): Array containing the covariate data.
         u_partition (list or np.array, optional): Partition of u for basis_funcs. Defaults to None.
         analytical_integration (bool, optional): Whether to use analytical integration. Defaults to False.
@@ -40,22 +42,30 @@ def estimation(
     u_partition = _compute_u_partition(basis_func_type, instrument["pscore"])
     basis_funcs = _generate_basis_funcs(basis_func_type, u_partition)
 
-    lp_inputs = {}
+    lp_first_inputs = {}
+    # The program is of the form
+    # min sum_s |tau_s(m) - beta_s|
+    # subject to basis funcs in [0, 1]
+    #
+    lp_first_inputs["c"] = _estimate_first_lp_weights(targets, basis_funcs)
+    lp_first_inputs["b_eq"] = _estimate_identified_estimands(
+        identified_estimands, y_data, z_data, d_data
+    )
+    lp_first_inputs["A_eq"] = []
 
     # First step linear program to find minimal deviations in constraint
     minimal_deviations = _solve_first_step_lp_estimation()
 
     # Second step linear program
-    lp_inputs["c"] = _compute_choice_weights(target, basis_funcs)
-    lp_inputs["b_eq"] = _estimate_identified_estimands(
-        identified_estimands, y_data, z_data, d_data
-    )
-    lp_inputs["A_eq"] = _estimate_equality_constraint_matrix(
+    lp_second_inputs = {}
+    lp_second_inputs["b_eq"] = minimal_deviations + tolerance
+    lp_second_inputs["c"] = _compute_choice_weights(target, basis_funcs)
+    lp_second_inputs["A_eq"] = _estimate_equality_constraint_matrix(
         identified_estimands, basis_funcs, instrument=instrument
     )
 
-    upper_bound = (-1) * _solve_second_step_lp_estimation(lp_inputs, "max").fun
-    lower_bound = _solve_second_step_lp_estimation(lp_inputs, "min").fun
+    upper_bound = (-1) * _solve_second_step_lp_estimation(lp_second_inputs, "max").fun
+    lower_bound = _solve_second_step_lp_estimation(lp_second_inputs, "min").fun
 
     return {"upper_bound": upper_bound, "lower_bound": lower_bound}
 
@@ -139,4 +149,83 @@ def _estimate_estimand(estimand, y_data, z_data, d_data):
 
 def _estimate_equality_constraint_matrix():
     """Estimate the equality constraint matrix."""
+    pass
+
+
+def _estimate_first_lp_weights(
+    identified_estimands, basis_funcs, y_data, z_data, d_data
+):
+    """Estimate the choice weights for a set of identified estimands by computing the
+    weight matrix associated with each estimand and using linearity."""
+
+    weights = np.zeros(len(basis_funcs))
+
+    for estimand in identified_estimands:
+        weight_matrix += _estimate_weights_estimand(
+            estimand, basis_funcs, z_data, d_data
+        )
+
+    return weights
+
+
+def _estimate_weights_estimand(estimand, basis_funcs, z_data, d_data):
+    """Estimate the weights on each basis function for a single estimand."""
+
+    estimand_type = estimand["type"]
+
+    z_p = _generate_array_of_p_scores(z_data, d_data)
+
+    if estimand_type == "ols_slope":
+        var_d = np.var(d_data)
+        ed = np.mean(d_data)
+
+        def s(d, z):
+            return s_ols_slope(d, ed=ed, var_d=var_d)
+
+    if estimand_type == "iv_slope":
+        ez = np.mean(z_data)
+        ed = np.mean(d_data)
+        edz = np.mean(d_data * z_data)
+        cov_dz = edz - ed * ez
+
+        def s(d, z):
+            return s_iv_slope(z, ez=ez, cov_dz=cov_dz)
+
+    if estimand_type == "cross":
+
+        def s(d, z):
+            return s_cross(d, z, dz_cross=estimand["dz_cross"])
+
+    weights = []
+
+    for basis_func in basis_funcs:
+        d_value = basis_func["d_value"]
+        u_lo = basis_func["u_lo"]
+        u_hi = basis_func["u_hi"]
+
+        # TODO potential bug: u_hi - u_lo for both cases?
+        if d_value == 0:
+            val = np.mean(
+                # FIXME check whether d_val or d_data here
+                (u_lo > z_p)
+                * s(d_data, z_data)
+                * (u_hi - u_lo)
+            )
+
+        elif d_value == 1:
+            val = np.mean(
+                # FIXME check whether d_val or d_data here
+                (u_hi <= z_p)
+                * s(d_data, z_data)
+                * (u_hi - u_lo)
+            )
+
+        weights.append(val)
+
+    return weights
+
+
+def _generate_array_of_p_scores(z_data, d_data):
+    """For input data on instrment and treatment generates array of same length with
+    estimated propensity scores for each corresponding entry of z."""
     pass
