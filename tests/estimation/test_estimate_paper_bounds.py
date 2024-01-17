16c4e56de14eec8519a9e87e3286ca5a9b0ba44e
buddejul <buddejul@gmail.com>
2024-01-17 15:47:21 +0100

    Adding parametrized tests for paper bounds figures 2,3, and 5.

---
+++ tests/estimation/test_estimate_paper_bounds.py
@@ -0,0 +1,45 @@
+"""Test we consistently estimate the bounds of the paper."""
+import numpy as np
+import pandas as pd  # type: ignore
+from pyvmte.estimation import estimation
+from pyvmte.simulation.simulation_funcs import monte_carlo_pyvmte
+
+import pytest
+
+from pyvmte.config import (
+    SETUP_FIG2,
+    SETUP_FIG3,
+    SETUP_FIG5,
+)
+
+RNG = np.random.default_rng(495618721)
+
+
+SAMPLE_SIZE = 10_000
+NUM_SIMULATIONS = 1000
+
+
+@pytest.mark.parametrize(
+    "setup",
+    [(SETUP_FIG2), (SETUP_FIG3), (SETUP_FIG5)],
+    ids=["fig2", "fig3", "fig5"],
+)
+def test_consistently_estimate_figure_bounds(setup):
+    expected = [setup["lower_bound"], setup["upper_bound"]]
+
+    results = monte_carlo_pyvmte(
+        sample_size=SAMPLE_SIZE,
+        repetitions=NUM_SIMULATIONS,
+        target=setup["target"],
+        identified_estimands=setup["identified_estimands"],
+        basis_func_type="constant",
+        tolerance=1 / np.sqrt(SAMPLE_SIZE),
+        rng=RNG,
+    )
+
+    mean_upper_bound = np.mean(results["upper_bounds"])
+    mean_lower_bound = np.mean(results["lower_bounds"])
+
+    actual = [mean_lower_bound, mean_upper_bound]
+
+    assert actual == pytest.approx(expected, abs=5 / np.sqrt(SAMPLE_SIZE))
--- tests/estimation/test_estimation_lp_inputs.py
+++ tests/estimation/test_estimation_lp_inputs.py
@@ -2,12 +2,14 @@
 import numpy as np
 import pandas as pd  # type: ignore
 import pytest
-from pyvmte.config import TEST_DIR
+from pyvmte.config import SETUP_FIG2, SETUP_FIG3, SETUP_FIG5
 
 from pyvmte.estimation.estimation import (
     _build_first_step_ub_matrix,
     _generate_basis_funcs,
     _estimate_prop_z,
+    _build_first_step_ub_matrix,
+    _compute_u_partition,
 )
 from pyvmte.identification.identification import _compute_equality_constraint_matrix
 from pyvmte.utilities import simulate_data_from_paper_dgp, load_paper_dgp
@@ -54,10 +56,14 @@ OLS_SLOPE_WEIGHTS = OLS_SLOPE_WEIGHTS
 IV_SLOPE_WEIGHTS = IV_SLOPE_WEIGHTS
 CROSS_WEIGHTS = CROSS_WEIGHTS
 
+SAMPLE_SIZE = 1_000
+REPETITIONS = 1_000
 
+
+@pytest.mark.skip("skip for now")
 def test_build_first_step_ub_matrix_consistency_ols_slope():
-    repetitions = 1_000
-    sample_size = 10_000
+    repetitions = REPETITIONS
+    sample_size = SAMPLE_SIZE
 
     data = simulate_data_from_paper_dgp(sample_size, rng=RNG)
 
@@ -96,9 +102,10 @@ def test_build_first_step_ub_matrix_consistency_ols_slope():
     assert actual == pytest.approx(OLS_SLOPE_WEIGHTS, abs=5 / np.sqrt(sample_size))
 
 
+@pytest.mark.skip("skip for now")
 def test_build_first_step_ub_matrix_consistency_iv_slope():
-    repetitions = 1_000
-    sample_size = 10_000
+    repetitions = REPETITIONS
+    sample_size = SAMPLE_SIZE
 
     data = simulate_data_from_paper_dgp(sample_size, rng=RNG)
 
@@ -137,9 +144,10 @@ def test_build_first_step_ub_matrix_consistency_iv_slope():
     assert actual == pytest.approx(IV_SLOPE_WEIGHTS, abs=7 / np.sqrt(sample_size))
 
 
+@pytest.mark.skip("skip for now")
 def test_build_first_step_ub_matrix_consistency_cross():
-    repetitions = 1_000
-    sample_size = 10_000
+    repetitions = REPETITIONS
+    sample_size = SAMPLE_SIZE
 
     data = simulate_data_from_paper_dgp(sample_size, rng=RNG)
 
@@ -176,3 +184,60 @@ def test_build_first_step_ub_matrix_consistency_cross():
     print(f"Expected: {CROSS_WEIGHTS}")
 
     assert actual == pytest.approx(CROSS_WEIGHTS, abs=5 / np.sqrt(sample_size))
+
+
+@pytest.mark.parametrize(
+    "setup",
+    [(SETUP_FIG2), (SETUP_FIG3), (SETUP_FIG5)],
+    ids=["fig2", "fig3", "fig5"],
+)
+def test_first_step_lp_A_ub_matrix_paper_figures(setup):
+    target = setup["target"]
+    identified_estimands = setup["identified_estimands"]
+    if type(identified_estimands) is not list:
+        identified_estimands = [identified_estimands]
+
+    actual = np.zeros(
+        (2 * len(identified_estimands), 2 * (len(BFUNCS)) + len(identified_estimands))
+    )
+    expected = np.zeros(
+        (2 * len(identified_estimands), 2 * (len(BFUNCS)) + len(identified_estimands))
+    )
+
+    for _ in range(REPETITIONS):
+        data = simulate_data_from_paper_dgp(sample_size=SAMPLE_SIZE, rng=RNG)
+
+        pscore_z = _estimate_prop_z(z_data=data["z"], d_data=data["d"])
+        u_partition = _compute_u_partition(
+            target=target, pscore_z=pscore_z, identified_estimands=identified_estimands
+        )
+        basis_funcs = _generate_basis_funcs("constant", u_partition)
+        A_ub = _build_first_step_ub_matrix(
+            basis_funcs=basis_funcs,
+            identified_estimands=identified_estimands,
+            d_data=data["d"],
+            z_data=data["z"],
+        )
+        # Sometimes we exactly estimate the pscore and then we have fewer bfuncs
+        # Require shape of actual and A_ub coincides for update
+        if actual.shape == A_ub.shape:
+            # FIXME this doesn't work because we start with 0
+            # continuously compute weighted average
+            actual += A_ub
+
+        weights = _compute_equality_constraint_matrix(
+            identified_estimands=identified_estimands,
+            basis_funcs=basis_funcs,
+            instrument=INSTRUMENT,
+        )
+
+        expected_upper = np.hstack((weights, -np.eye(len(identified_estimands))))
+        expected_lower = np.hstack((-weights, -np.eye(len(identified_estimands))))
+        expected_add = np.vstack((expected_upper, expected_lower))
+        if expected.shape == expected_add.shape:
+            expected += expected_add
+
+    expected /= REPETITIONS
+    actual /= REPETITIONS
+
+    assert actual == pytest.approx(expected, abs=3 / np.sqrt(SAMPLE_SIZE))
